142c2116d303a7634d989c0e86d4ef719064a06f
Vladislav-S <vovernak@gmail.com>
2016-12-04 18:08:51 +0300

    review 1

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,40 +1,31 @@
-#include "mainwindow.h"
-#include "ui_mainwindow.h"
-#include "reg.h"
-#include "database.h"
-#include "maincontent.h"
-#include <QDebug>
-
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
-{
-
+@@ -13,12 +13,14 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->setupUi(this);
     pContent = new MainContent();
     sReg = new Reg();
+    db = new DataBase();
+
     connect(pContent, &MainContent::firstWindow, this, &MainWindow::show);
     connect(sReg, &Reg::firstWindow, this, &Reg
             ::show);
+    connect(sReg, &Reg::firstWindow, this, &Reg::show);
     connect(this, SIGNAL(on_return()), ui->pushButton, SLOT(click()));
     log.clear();
     str.clear();
+    //строки и так пустые
+    //log.clear();
+    //str.clear();
 
 }
 
-MainWindow::~MainWindow()
+@@ -26,6 +28,7 @@ MainWindow::~MainWindow()
 {
     delete ui;
     delete pContent;
+    //можно выйти из программы не нажав кнопку 3
     delete db;
     delete sReg;
 }
-
-void MainWindow::on_pushButton_2_clicked()
-{
-    sReg->show();
-    this->close();
-}
+@@ -38,7 +41,6 @@ void MainWindow::on_pushButton_2_clicked()
 
 void MainWindow::on_pushButton_3_clicked()
 {
@@ -42,52 +33,3 @@ void MainWindow::on_pushButton_3_clicked()
     db->show();
 }
 
-void MainWindow::on_pushButton_clicked()
-{
-    QString Log;
-
-    Log=ui->lineEdit->text();
-
-    if(db->checkLogPas(Log,str)==true){
-        log=Log;
-        pContent->setLogPas(log,str);
-        pContent->show();
-        pContent->setFnameLname(Log);
-        this->close();
-        ui->lineEdit->clear();
-        ui->lineEdit_2->clear();
-
-    }
-    else{
-        ui->label_4->setText("Please check login or password");
-    }
-    log.clear();
-    str.clear();
-
-}
-
-void MainWindow::on_lineEdit_2_textChanged(const QString &arg1)
-{
-    if(sTextchanged){
-        int itcoutn=arg1.length();
-        QString arg2 = arg1;
-        QString arg3 = arg1;
-        if(str.size() == itcoutn+1){
-            str.resize(str.size()-1);
-        }
-        else str += arg2.remove("*");
-
-        arg3.replace(QRegularExpression("."), "*");
-        sTextchanged = false;
-        ui->lineEdit_2->setText(arg3);
-    }
-    sTextchanged = true;
-
-}
-
-
-
-void MainWindow::on_lineEdit_2_returnPressed()
-{
-    emit on_return();
-}
